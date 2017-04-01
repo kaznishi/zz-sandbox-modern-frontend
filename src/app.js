d8580c008e01666472799087b3f0b12782e70ce3
kaznishi <kaz.nishimu@gmail.com>
2017-04-01 10:05:12 +0900

    src commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+var css = {
+    style: require('./scss/style.scss')
+};
+
+console.log('hello, world');
+console.log('hello, world2');
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+ module.exports = [{
+     entry: ['./src/app.js'],
+     output: {
+         filename: 'bundle.js',
+         path: path.resolve(__dirname, 'dist')
+     },
+     module: {
+         rules: [{
+             test: /\.(css|sass|scss)$/,
+             use: [
+                 'style-loader',
+                 'css-loader',
+                 'sass-loader'
+             ]
+         }]
+     }
+ }];
